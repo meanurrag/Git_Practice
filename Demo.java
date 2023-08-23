60816f4b509480080030ec344370653bbbd9b402
anurag <meanuragranjan@gmail.com>
2023-08-23 20:23:35 +0530

    feature added

--- Demo.java
+++ Demo.java
@@ -2,4 +2,8 @@ public class Hello	{
 	public static void main(String[] args)	{
 		System.out.println("Demo Program");
 	}
+	//Adding some features
+	public static int sum(int a, int b)	{
+		return a+b;
+	}
 }
